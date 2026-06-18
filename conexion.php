85cc7ed538b2f0fa5583f127fc51f6dfad4c4abe
carnecx <vincentquesada841@gmail.com>
2026-06-18 14:49:37 -0600

    Create conexion.php

---
+++ conexion.php
@@ -0,0 +1,14 @@
+<?php
+
+$host     = "localhost";
+$dbname   = "control_tareas";
+$usuario  = "root";
+$password = "admin123";
+
+$conn = new mysqli($host, $usuario, $password, $dbname);
+
+if ($conn->connect_error) {
+    die("Error al conectar con la base de datos: " . $conn->connect_error);
+}
+
+$conn->set_charset("utf8mb4");
