97786d382346c2156e5ced14dded11129667e89a
Reychell2307 <martinezreychell724@gmail.com>
2026-06-30 18:58:28 -0600

    .

--- conexion.php
+++ conexion.php
@@ -1,5 +1,4 @@
 <?php
-<<<<<<< Updated upstream
 
 $host     = "localhost";
 $dbname   = "control_tareas";
@@ -13,19 +12,3 @@ if ($conn->connect_error) {
 }
 
 $conn->set_charset("utf8mb4");
-=======
-$host = "localhost";
-$puerto = "3307";
-$usuario = "root";
-$password = "12345";
-$baseDatos = "control_tareas";
-
-$conexion = new mysqli($host, $usuario, $password, $baseDatos, $puerto);
-
-if ($conexion->connect_error) {
-    die("Error de conexión: " . $conexion->connect_error);
-}
-
-$conexion->set_charset("utf8mb4");
-?>
->>>>>>> Stashed changes
